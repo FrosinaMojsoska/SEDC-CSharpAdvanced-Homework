ad68d3ac54c7b7b45983c81015314a51d9c99273
Frosina Mojsoska <frosina_mojsoska@hotmail.com>
2019-04-11 15:53:44 +0200

    Homework2

---
+++ Homework2CSharpAdvanced/Task1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+   static class Program
+    {
+
+        public static void Frequency<T>(this  T [] array)
+        {
+            T [] newArray = array.Distinct().ToArray();
+            Console.WriteLine("The Frequency are:");
+            foreach (var item in newArray)
+            {
+                T[] result = array.Where(x => x.Equals(item)).ToArray();
+                Console.WriteLine($"{item} appears {result.Length} times");
+            }
+        }
+        #region NumFrequency with method
+        //public static void NumFrequency(int [] arrayInt)
+        //{
+        //    int[] newList = arrayInt.Distinct().ToArray();
+        //    Console.WriteLine("The number and the Frequency are :");
+        //    foreach (var item in newList)
+        //    {
+        //        int[] result = arrayInt.Where(x => x == item).ToArray();
+        //        Console.WriteLine($"Number {item} appears {result.Length} times");
+        //    }
+
+        //}
+        #endregion
+
+        #region CharFrequency with method
+        //public static void FString(string stringL)
+        //{
+        //    char[] listn = stringL.Distinct().ToArray();
+        //    Console.WriteLine("The frequency of the characters are :");
+        //    foreach (var item in listn)
+        //    {
+        //        char[] result = stringL.Where(x => x == item).ToArray();
+        //        Console.WriteLine($"Character {item}:  {result.Length} times");
+        //    }
+        //}
+        #endregion
+
+        static void Main(string[] args)
+        {
+            //int array
+            int[] arrayInt = new int[] { 1, 5, 5, 9, 5, 9 };
+            arrayInt.Frequency();
+           
+            //string
+            string stringL = "apple";
+            char[]stringArray=stringL.ToCharArray();
+            stringArray.Frequency();
+   
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework2CSharpAdvanced/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the number of members on the List: ");
+            int NumberMembers = Convert.ToInt32(Console.ReadLine());
+            List<int> ListInt = new List<int>();
+            for(int i=0;i<NumberMembers;i++)
+            {
+                Console.WriteLine($"Enter member {i} : ");
+                ListInt.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.Write("Input the value above you want to display the members of the List : ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            List<int> resultList = new List<int>();
+            resultList = ListInt.Where(x => x > value).ToList();
+            Console.WriteLine($"The numbers greater than {value} are:");
+            foreach (var item in resultList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
